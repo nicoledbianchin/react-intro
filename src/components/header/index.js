a8a16d3757c3c0fe2519fb31812f54224ae9a12a
nicoledbianchin <nicoledbianchin@gmail.com>
2020-02-15 15:59:14 -0300

    cria header

---
+++ src/components/header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => {
+    return <header id="main-header">JSHunt</header>
+}
+
+
+/* equivalente a:
+ class Header extends React.Component{
+   render(){
+         return blablabla
+     }
+ }*/
+
+ export default Header;
