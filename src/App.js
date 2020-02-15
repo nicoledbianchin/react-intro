2f7c0471a17da9b19764b8a798064dbd9ce2540c
nicoledbianchin <nicoledbianchin@gmail.com>
2020-02-15 15:30:47 -0300

    update read me

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import Tarefas from './Tarefas';
 
 export default class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <Tarefas/>
       </div>
     );
   }
