53df0fd5b4a8d96c826c22a2bdb4382ebe60e075
nicoledbianchin <nicoledbianchin@gmail.com>
2020-02-18 09:44:06 -0300

    adiciona main ao app

--- src/App.js
+++ src/App.js
@@ -4,15 +4,6 @@ import Main from './pages/main/index'
 import api from './service/api';
 
 export default class App extends Component {
-  componentDidMount() {
-    this.loadProducts();
-  }
-
-  loadProducts = async () => {
-    const response = await api.get('/products');
-
-    console.log(response.data.docs);
-  };
 
   render() {
     return (
