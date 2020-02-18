f3d18a245efa93161b419f05787a265ef6bcee4d
nicoledbianchin <nicoledbianchin@gmail.com>
2020-02-18 09:45:06 -0300

    constroi main

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,8 +1,40 @@
 import React from 'react'; 
 import api from '../../service/api'
+import './styles.css'
 
 export default class Main extends React.Component  {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            products: []
+        };
+    }
+
+    componentDidMount() {
+        this.loadProducts();
+    }
+    
+    loadProducts = async () => {
+        const response = await api.get('/products');
+        this.setState({
+            products: response.data.docs
+        });
+      };
+
     render() {
-        return <h1>Teste</h1>
+        
+        return (
+            <div className="product-list">
+                <h1>Contagem de produtos: {this.state.products.length}</h1>
+                    {this.state.products.map(product => (
+                        <article key={product._id}>
+                            <strong>{product.title}</strong>
+                            <p>product.description</p>
+                            <a href="">Acessar</a>
+                        </article>
+                    ))}
+            </div>
+        )
     }
 }
