bca9cb3ab63248715694a3584a94f031d1278b1c
nicoledbianchin <nicoledbianchin@gmail.com>
2020-02-18 09:08:55 -0300

    refatora css e termina ligacao com o back

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import Header from './components/header/index'
-import api from './service/api'
+import Main from './pages/main/index'
+import api from './service/api';
 
 export default class App extends Component {
+  componentDidMount() {
+    this.loadProducts();
+  }
+
+  loadProducts = async () => {
+    const response = await api.get('/products');
+
+    console.log(response.data.docs);
+  };
 
   render() {
     return (
       <div className="App">
       <Header/>
+      <Main/>
       </div>
     );
   }
---
+++ src/pages/main/index.js
@@ -0,0 +1,8 @@
+import React from 'react'; 
+import api from '../../service/api'
+
+export default class Main extends React.Component  {
+    render() {
+        return <h1>Teste</h1>
+    }
+}
